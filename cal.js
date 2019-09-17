e344c3c3f9809fe2950452c7a154047bed7def80
Benoit Martel <martel.b@gmail.com>
2019-09-16 21:01:54 -0400

    no-cors

--- cal.js
+++ cal.js
@@ -18,7 +18,7 @@ const months = [
 
 ical.fromURL(
   "https://calendar.google.com/calendar/ical/martel.b%40gmail.com/private-327e283d3a084fbaa7ee16a3f5e30468/basic.ics",
-  {},
+  { mode: "no-cors" },
   function(err, data) {
     console.log(Object.keys(data).length);
     for (let k in data) {
